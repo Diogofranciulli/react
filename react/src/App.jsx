61be8eb08909b6487ab93ab42bc8060d39a7c60a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 20:47:22 -0300

    palmeiras

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./header"
+
+function App() {
+
+
+  return (
+    <>
+    <Header/>
+     <h1 className="text-orange-600 text-5xl ml-7 mb-2 pl-3 flex justify-center md:text-green-400
+     w-[327px]
+     font-bold ">TITULO DA PAGINA</h1>
+    </>
+  )
+}
+
+export default App
---
+++ react/src/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header(){
+    return(
+        <h1>Header 2</h1>
+    )
+}
